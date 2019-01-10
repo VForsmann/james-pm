c9160b874258545c75a6b1fbd2500e8e03134b76
Timo Reimer <reimer@westphalia-datalab.com>
2019-01-10 03:44:01 +0100

    add task to backlog

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,18 +2,24 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { ReferenceService } from './reference.service';
 import { Observable } from 'rxjs';
+import { firestore } from 'firebase';
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
-  constructor(private db: AngularFirestore, private referenceService: ReferenceService) { }
+  constructor(
+    private db: AngularFirestore,
+    private referenceService: ReferenceService
+  ) {}
   selectedTasks;
   getTasks() {
     // Man muss zuerst die Documentreference holen um nach der Referenz zu selektieren
-    const backlogRef = this.referenceService.getBacklogReference('Jbhi2Z7NmyIZ4fNRCkRW');
-    return this.db.collection('tasks', ref => ref
-      .where('backlog', '==', backlogRef)).snapshotChanges();
+    const backlogRef = this.referenceService.getBacklogReference(
+      'Jbhi2Z7NmyIZ4fNRCkRW'
+    );
+    return this.db
+      .collection('tasks', ref => ref.where('backlog', '==', backlogRef))
+      .snapshotChanges();
   }
 
   updateTask(task) {
@@ -22,11 +28,44 @@ export class TaskService {
     // const user = this.db.collection('').doc('gApsvyNMc8zP3KtC6MNr').ref;
   }
 
+  getAllTasks(projectId) {
+    const projectRef = this.referenceService.getProjectReference(projectId);
+    return Observable.create(observer => {
+      this.db
+        .collection('tasks', ref => ref.where('project', '==', projectRef))
+        .snapshotChanges()
+        .subscribe(tasks_data => {
+          const tasks_list = [];
+          tasks_data.map(actions => {
+            const taskId = actions.payload.doc.id;
+            const subs = this.getTaskWithId(taskId).subscribe(task_data => {
+              let update_task = tasks_list.map(t => t.id);
+                console.log('wtf');
+                if (tasks_data) {
+                  task_data['id'] = taskId;
+                  if (update_task.indexOf(taskId) !== -1) {
+                    tasks_list[tasks_list.indexOf(taskId)] = task_data;
+                  } else if (task_data['backlog'] === undefined) {
+                    tasks_list.push(task_data);
+                  }
+                } else {
+                  tasks_list.splice(update_task.indexOf(taskId), 1);
+                }
+                observer.next(tasks_list);
+                update_task = [];
+            });
+          });
+        });
+    });
+  }
+
   getTasksForBacklog(backlogId) {
     const backlogRef = this.referenceService.getBacklogReference(backlogId);
     return Observable.create(observer => {
-      const tasks = this.db.collection('tasks', ref => ref
-        .where('backlog', '==', backlogRef)).snapshotChanges().subscribe(tasks_data => {
+      const tasks = this.db
+        .collection('tasks', ref => ref.where('backlog', '==', backlogRef))
+        .snapshotChanges()
+        .subscribe(tasks_data => {
           const tasks_list = [];
           tasks_data.map(actions => {
             const taskId = actions.payload.doc.id;
@@ -39,6 +78,7 @@ export class TaskService {
                 } else {
                   tasks_list.push(task_data);
                 }
+                task.unsubscribe();
                 observer.next(tasks_list);
               }
             });
@@ -53,5 +93,24 @@ export class TaskService {
       .doc(taskId)
       .valueChanges();
     return result;
+  }
+
+  addTaskToBacklog(task, backlogId) {
+    task['backlog'] = this.referenceService.getBacklogReference(backlogId);
+    console.log(task);
+    return this.db
+      .collection('tasks')
+      .doc(task['id'])
+      .update(task);
+  }
+
+  removeTaskFromBacklog(task, backlogId) {
+    // task['backlog'] = this.referenceService.getBacklogReference(backlogId);
+    // console.log(task);
+    // return this.db
+    //   .collection('tasks')
+    //   .doc(task['id']).update({
+    //     backlog: firebase.firestore.FieldValue.delete();
+    // });
   }
 }
--- src/app/views/backlog/backlog-tasks/backlog-tasks.component.ts
+++ src/app/views/backlog/backlog-tasks/backlog-tasks.component.ts
@@ -11,12 +11,32 @@ import { Observable } from 'rxjs';
 })
 export class BacklogTasksComponent implements OnInit {
   backlogId: string;
+  projectId: string;
   tasks: Observable<{}>;
+  allTasks: Observable<{}>;
   constructor(private route: ActivatedRoute, private referenceService: ReferenceService, private taskService: TaskService) { }
 
   ngOnInit() {
     this.backlogId = this.route.snapshot.paramMap.get('bid');
     this.tasks = this.taskService.getTasksForBacklog(this.backlogId);
+    this.projectId = this.route.snapshot.paramMap.get('id');
+    this.allTasks = this.taskService.getAllTasks(this.projectId);
   }
 
+  onDropAdd(e: any) {
+    // e.dragdata entspricht dem aktuellen task den man an die methode übergibt
+    this.taskService.addTaskToBacklog(e.dragData, this.backlogId).then(res => {
+      console.log('task update');
+    });
+    // Nur zum testen, nicht aufregen (Ändert den Status von dem Task) name => Status bzw. name vom Balken
+    // e => Task den man bewegt
+    // e.dragData.status = this.name;
+  }
+
+  onDropRemove(e: any) {
+    // e.dragdata entspricht dem aktuellen task den man an die methode übergibt
+    // this.taskService.removeTaskFromBacklog(e.dragData, this.backlogId).then(res => {
+    //   console.log('task update');
+    // });
+  }
 }
