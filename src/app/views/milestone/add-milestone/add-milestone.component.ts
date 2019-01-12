af2fa170b6b7138b83a375c7bac11a54324c6664
Tim Sawatzki <tsawatzki@fh-muenster.de>
2019-01-12 11:39:19 +0100

    Adding new milestones works

--- src/app/views/milestone/add-milestone/add-milestone.component.ts
+++ src/app/views/milestone/add-milestone/add-milestone.component.ts
@@ -3,6 +3,7 @@ import { NgbActiveModal, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { MilestoneAngular } from 'src/app/model/milestone';
 import { MilestoneService } from 'src/app/services/milestone.service';
 import { DatepickerComponent } from 'src/app/shared/datepicker/datepicker.component';
+import { firestore } from 'firebase';
 
 @Component({
   selector: 'app-add-milestone',
@@ -24,11 +25,15 @@ export class AddMilestoneComponent implements OnInit {
   }
 
   onSubmit(){
+    this.milestoneService.addMilestone({
+      name: this.milestone.name,
+      description: this.milestone.description,
+      done: firestore.Timestamp.fromDate(this.milestone.done)
+    })
     this.activeModal.close();
   }
 
   datepicked(datepickerDate: NgbDateStruct){
-    console.log(new Date(datepickerDate.year, datepickerDate.month - 1, datepickerDate.day));
+    this.milestone.done = new Date(datepickerDate.year, datepickerDate.month - 1, datepickerDate.day);
   }
-
 }
