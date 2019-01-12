ae4ce71bff153510569f9be0fa3f537dd5abc9b4
Tim Sawatzki <tsawatzki@fh-muenster.de>
2019-01-12 12:38:29 +0100

    Modified roadmap

--- src/app/views/roadmap/roadmap.component.ts
+++ src/app/views/roadmap/roadmap.component.ts
@@ -3,6 +3,7 @@ import { MilestoneService } from 'src/app/services/milestone.service';
 import { Observable } from 'rxjs';
 import { MilestoneFirebase } from 'src/app/model/milestone';
 import { AddMilestoneComponent } from '../milestone/add-milestone/add-milestone.component';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-roadmap',
@@ -13,6 +14,9 @@ export class RoadmapComponent implements OnInit{
 
   milestones: Observable<MilestoneFirebase[]>;
   addMilestoneComponent = AddMilestoneComponent;
+
+  faEdit = faEdit;
+  faTrash = faTrash;
 
   constructor(private milestoneService: MilestoneService) {}
 
